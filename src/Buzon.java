2842ac89e019f9db609084971928717f98c308f5
Juan José Osorio <j.osorioc@uniandes.edu.co>
2022-08-23 15:04:41 -0500

    Clase de Buzón y Proceso

---
+++ src/Buzon.java
@@ -0,0 +1,10 @@
+
+/**
+ * Hay 8 buzones en total. 6 intermedios tienen el mismo tamaño. 2 tienen 3 veces el tamaño, siendo
+ * el de entrada y de salida.
+ */
+public class Buzon {
+
+    private int capacidad; // Ni idea que debe hacer la capacidad
+
+}
---
+++ src/Proceso.java
@@ -0,0 +1,9 @@
+/**
+ * El primer proceso solo tiene un buzón de salida y el último proceso solo tiene un buzón de
+ * salida.
+ */
+public class Proceso extends Thread {
+    // Buzón de entrada.
+
+    // Buzón de salida.
+}
